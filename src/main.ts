2dc8fd10ab2c20006bb7e12aec4c52f6a3586879
kaikdorvalo <kaikdorvalo@gmail.com>
2023-10-11 11:19:57 -0300

    ajuste de cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { AppModule } from './app.module';
 require('dotenv').config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ["http://localhost:5173", "http://localhost:8000", "http://localhost:4200"]
-  })
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  app.enableCors(options);
   await app.listen(process.env.PORT);
 }
 bootstrap();
