6dc144348ae3cd5873383904bea32f7129f1a5bf
kaikdorvalo <kaikdorvalo@gmail.com>
2023-10-11 11:14:59 -0300

    ajuste para porta 8080

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ["http://localhost:5173", "http://localhost:8000", "http://localhost:4200"]
   })
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
